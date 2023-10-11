75148397a44355d13f1b99ed39551e3d3d734b7a
Jose Badillo <utm20040072@utma.edu.mx>
2023-10-10 19:26:01 -0600

    add the to do list

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,15 +2,16 @@ import { useState } from "react";
 
 function ListItem ({text, checked, onDelete})
 {
-    const[ setChecked] = useState(false);
+    const[setChecked] = useState(false);
     const handleCheck =() =>
         {
             setChecked(!checked);
         }
     return (
-            <li className={`"p-2 bg-indigo-800 ${checked ? 'line-through':null}"`}>
+            <li className={`rounded-md "p-3 bg-indigo-800 ${checked ? 'line-through':null}"`}>
                 {text}
-            <input type="checkbox" checked={checked} onChange={handleCheck} /></li>
+            <input type="checkbox" checked={checked} onChange={handleCheck}/>
+            </li>
             );
 }
 
